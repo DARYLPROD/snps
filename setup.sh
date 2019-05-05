b3d905ba0e9e03ffca2eb6a1ed50866a36eae3e8
DARYLPROD <45126850+DARYLPROD@users.noreply.github.com>
2019-05-05 15:38:58 +0200

    Add files via upload

--- setup.sh
+++ setup.sh
@@ -22,3 +22,14 @@ then
             # If a Python2 environment exists, delete it first
             # before creating a new Python 3 virtual environment.
             rm -r $VENV
+            $PYTHON -m venv $VENV
+        fi
+    fi
+
+    # Activate the virtual environment and install requirements.
+    . $VENV/bin/activate
+    pip3 install -r requirements.txt
+
+else
+    >&2 echo "Cannot find Python 3. Please install it."
+fi
